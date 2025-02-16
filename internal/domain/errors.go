a0184b2981c9ffb64dfd41715ce38f0de6d1e662
Nikolay Popov <helioproduct.sync@gmail.com>
2025-02-16 15:19:28 +0300

    transfer usecase: SendCoins

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -2,7 +2,21 @@ package domain
 
 import "errors"
 
+// Users
 var (
-	ErrUserNotFound    = errors.New("user not found")
+	ErrUserNotFound = errors.New("user not found")
+)
+
+// Products
+var (
 	ErrProductNotFound = errors.New("product not found")
+)
+
+// Trasfers
+var (
+	ErrZeroAmount          = errors.New("amount must be greater than zero")
+	ErrSameUser            = errors.New("cannot send coins to yourself")
+	ErrMissingFromUser     = errors.New("sender user ID must be specified")
+	ErrMissingToUser       = errors.New("recipient user ID must be specified")
+	ErrInsufficientBalance = errors.New("insufficient balance")
 )
--- internal/usecase/transfer/send_coins.go
+++ internal/usecase/transfer/send_coins.go
@@ -2,7 +2,11 @@ package transfer
 
 import (
 	"context"
+	"fmt"
 	"shop-service/internal/domain"
+	"shop-service/internal/repository/user"
+
+	"github.com/rs/zerolog/log"
 )
 
 type SendCoinsRequest struct {
@@ -12,11 +16,98 @@ type SendCoinsRequest struct {
 }
 
 func (uc *TransferUsecase) SendCoins(ctx context.Context, req SendCoinsRequest) error {
+	caller := "TransferUsecase.SendCoins"
 
-	uc.trm.Do(ctx, func(ctx context.Context) error {
+	err := req.Validate()
+	if err != nil {
+		log.Err(err).Str("caller", caller).Msg("validation failed")
+		return err
+	}
+
+	return uc.trm.Do(ctx, func(ctx context.Context) error {
+		log.Debug().Str("caller", caller).Msg("starting transaction")
+
+		sender, err := uc.userRepo.GetUserByID(ctx, req.From)
+		if err != nil {
+			err = fmt.Errorf("error getting sender: %w", err)
+			log.Err(err).Str("caller", caller).Send()
+			return err
+		}
+
+		recipient, err := uc.userRepo.GetUserByID(ctx, req.To)
+		if err != nil {
+			err = fmt.Errorf("error getting recipient: %w", err)
+			log.Err(err).Str("caller", caller).Send()
+			return err
+		}
+
+		if sender.Balance < req.Amount {
+			log.Error().Str("caller", caller).Msg("insufficient balance")
+			return domain.ErrInsufficientBalance
+		}
+
+		newSenderBalance := sender.Balance - req.Amount
+		err = uc.userRepo.UpdateUser(ctx, user.UpdateUserRequest{
+			UserID:  sender.ID,
+			Balance: &newSenderBalance,
+		})
+		if err != nil {
+			err = fmt.Errorf("failed to update sender balance: %w", err)
+			log.Err(err).Str("caller", caller).Send()
+			return err
+		}
+
+		newRecipientBalance := recipient.Balance + req.Amount
+		err = uc.userRepo.UpdateUser(ctx, user.UpdateUserRequest{
+			UserID:  recipient.ID,
+			Balance: &newRecipientBalance,
+		})
+		if err != nil {
+			err = fmt.Errorf("failed to update recipient balance: %w", err)
+			log.Err(err).Str("caller", caller).Send()
+			return err
+		}
+
+		transfer := domain.Transfer{
+			From:   req.From,
+			To:     req.To,
+			Amount: req.Amount,
+		}
+
+		_, err = uc.transferRepo.CreateTransfer(ctx, transfer)
+		if err != nil {
+			err = fmt.Errorf("failed to create transfer record: %w", err)
+			log.Err(err).Str("caller", caller).Send()
+			return err
+		}
+
+		log.Info().
+			Str("caller", caller).
+			Str("from_user", sender.Username).
+			Str("to_user", recipient.Username).
+			Uint64("amount", req.Amount).
+			Msg("Transfer completed successfully")
 
 		return nil
 	})
+}
+
+func (req *SendCoinsRequest) Validate() error {
+	if req.Amount == 0 {
+		return domain.ErrZeroAmount
+	}
+
+	if req.From == req.To {
+		return domain.ErrSameUser
+	}
+
+	if req.From == 0 {
+		return domain.ErrMissingFromUser
+	}
+
+	if req.To == 0 {
+		return domain.ErrMissingToUser
+	}
 
 	return nil
 }
